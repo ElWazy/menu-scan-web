3938afbbed503fe4cde86bc9ea861e6f44aabf7d
ElWazy <ElCazero.Fer@gmail.com>
2023-08-10 00:01:04 -0400

    update: habemus conexion con el backend

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,31 +1,31 @@
 <template>
-    <section class="page-section bg-light" id="portfolio">
-      <div class="container">
-        <div class="text-center">
-          <h2 class="section-heading text-uppercase">Products section</h2>
-          <!-- <h3 class="section-subheading text-muted">Todos las comidas que quieras probar.</h3> -->
-        </div>
-        <div class="row" id="alimentos-container">
-
-            <div class="col-lg-4 col-sm-6 mb-4">
-              <!-- Portfolio item 1 -->
-              <div class="portfolio-item">
-                <a class="portfolio-link" data-bs-toggle="modal">
-                  <img class="" src="@/assets/images/shiawi.jpg" height="300px" width="100%"
-                    alt="..." />
-                </a>
-                <div class="portfolio-caption">
-                  <div class="portfolio-caption-heading">
-                    El parrillero legendario 😎
-                  </div>
-                  <div class="portfolio-caption-subheading text-muted fs-4">$ 300.000.000 CLP</div>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
-    </section>
+  <div>
+    <ul>
+      <li v-for="item in data" :key="item.id">
+        <img :src="item.image" width="32" height="32" />
+        {{ item.name }}</li>
+    </ul>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// Estado reactivo para almacenar los datos
+const data = ref([])
+
+// eslint-disable-next-line no-unused-vars
+const fetchData = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/users')
+    const jsonData = await response.json()
+    data.value = jsonData
+  } catch (error) {
+    console.error('Error al cargar los datos:', error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
